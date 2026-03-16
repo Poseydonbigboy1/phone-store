45e575858bb688662ddd77b42e94ce1ccfcdced4
RomanShebanic <roma.shebanic2@gmail.com>
2026-03-16 21:20:50 +0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './views/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'users',
+        component: UsersComponent
+    }, {
+        path: 'users',
+        component: UsersComponent
+    }];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BackendRequestsModule } from './core/backend/backend-requests.module';
+import { UsersComponent } from './views/users/users.component';
 
 @Component({
   selector: 'app-root',
