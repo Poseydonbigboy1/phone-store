3810bf37fa8e349370ee75f89ed64d7c37cee68f
RomanShebanic <roma.shebanic2@gmail.com>
2026-03-20 00:37:34 +0300

    fix

--- src/app/views/site-layuot-page/profile-page/profile-page.ts
+++ src/app/views/site-layuot-page/profile-page/profile-page.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Nullable } from '@models/common';
+import { User } from '@models/data';
+import { AuthService } from '@services';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-profile-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile-page.html',
   styleUrl: './profile-page.scss',
+  standalone: true,
 })
-export class ProfilePage {}
+export class ProfilePage {
+  user$: Observable<Nullable<User>>;
+
+  private readonly authService: AuthService;
+
+  constructor() {
+    this.authService = inject(AuthService);
+
+    this.user$ = this.authService.user$ as Observable<Nullable<User>>;
+  }
+}
--- src/app/views/site-layuot-page/site-layuot-page.ts
+++ src/app/views/site-layuot-page/site-layuot-page.ts
@@ -1,11 +1,58 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from '@models/data';
+import { AuthService } from '@services';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { Nullable } from 'primeng/ts-helpers';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-site-layuot-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule, CommonModule],
   templateUrl: './site-layuot-page.html',
   styleUrl: './site-layuot-page.scss',
+  standalone: true,
 })
-export class SiteLayuotPage {}
+export class SiteLayuotPage {
+  menuItems: MenuItem[] = [];
+  user$: Observable<Nullable<User>>;
+
+  private readonly authService: AuthService;
+
+  constructor() {
+    this.authService = inject(AuthService);
+
+    this.user$ = this.authService.user$;
+  }
+
+  ngOnInit() {
+    this.user$.subscribe((user) => {
+      this.updateMenu(!!user);
+    });
+  }
+
+  updateMenu(isLoggedIn: boolean) {
+    this.menuItems = [
+      {
+        label: 'Главная',
+        icon: 'pi pi-home',
+        routerLink: '/',
+        routerLinkActiveOptions: { exact: true },
+      },
+      { label: 'Каталог', icon: 'pi pi-list', routerLink: '/main/products' },
+      {
+        label: 'Корзина',
+        icon: 'pi pi-shopping-cart',
+        routerLink: '/main/checkout',
+        styleClass: 'ml-auto',
+      },
+      {
+        label: isLoggedIn ? 'Профиль' : 'Войти',
+        icon: 'pi pi-user',
+        routerLink: isLoggedIn ? '/main/profile' : '/login',
+      },
+    ];
+  }
+}
