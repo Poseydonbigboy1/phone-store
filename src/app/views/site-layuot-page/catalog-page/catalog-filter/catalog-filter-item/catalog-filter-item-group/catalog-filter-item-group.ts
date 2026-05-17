0146e03edcc4cf536d1dbee78358a8d356b3acf7
MACTEPwar <shebanic95@gmail.com>
2026-05-17 19:25:28 +0300

    feat(catalog-filter): Верстка

--- src/app/views/site-layuot-page/catalog-page/catalog-filter/catalog-filter-item/catalog-filter-item-group/catalog-filter-item-group.ts
+++ src/app/views/site-layuot-page/catalog-page/catalog-filter/catalog-filter-item/catalog-filter-item-group/catalog-filter-item-group.ts
@@ -1,10 +1,11 @@
 import { CommonModule, NgComponentOutlet } from '@angular/common';
 import { Component, Input, Type } from '@angular/core';
 import { CatalogFilterItem } from '../../catalog-filter-item/catalog-filter-item';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-catalog-filter-item-group',
-  imports: [CommonModule, NgComponentOutlet],
+  imports: [CommonModule, NgComponentOutlet, DividerModule],
   templateUrl: './catalog-filter-item-group.html',
   styleUrl: './catalog-filter-item-group.scss',
 })
--- src/app/views/site-layuot-page/catalog-page/catalog-filter/catalog-filter-item/catalog-filter-item.ts
+++ src/app/views/site-layuot-page/catalog-page/catalog-filter/catalog-filter-item/catalog-filter-item.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { CatalogFilterItemGroup } from './catalog-filter-item-group/catalog-filter-item-group';
 import { CatalogFilterItemCheckbox } from './catalog-filter-item-checkbox/catalog-filter-item-checkbox';
 import { CatalogFilterItemRangeNumber } from './catalog-filter-item-range-number/catalog-filter-item-range-number';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-catalog-filter-item',
